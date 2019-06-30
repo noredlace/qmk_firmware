97525abb2e83f7ca2ba6ad90c2f13e8415de70f4
noredlace <ejcalderon@gmail.com>
2019-06-29 23:13:41 -0500

    update keymap for mechmini

--- keyboards/mechmini/v2/keymaps/lizard/keymap.c
+++ keyboards/mechmini/v2/keymaps/lizard/keymap.c
@@ -1,28 +1,56 @@
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FN1 1
-#define _FN2 2
+
+
+enum mm2_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _FUNCTION,
+  _ADJUST
+};
+
+enum mm2_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  FUNCTION,
+};
+
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BL] = LAYOUT_ortho(
-		KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  	KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
- 	 	KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-  	RGB_TOG,  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
-	[_FN1] = LAYOUT_ortho(
-		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, KC_BSLS, KC_DEL,
-		KC_TRNS, KC_TRNS, KC_TRNS, M(1), M(1), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
+[_QWERTY] = LAYOUT_ortho(
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+	KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+  FUNCTION, KC_LCTL, KC_LGUI, KC_LALT,LOWER,   KC_SPC,  KC_ENT,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
-	[_FN2] = LAYOUT_ortho(
-		KC_PWR, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
-		KC_TRNS, BL_TOGG, BL_STEP, BL_INC, BL_DEC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
-		KC_TRNS, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, KC_HOME, KC_END, KC_TRNS,
-		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY),
+	[_RAISE] = LAYOUT_ortho(
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR,    KC_PAST,    KC_MINS,  KC_EQL,   KC_BSPC,
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_LEFT, KC_DOWN,    KC_UP,      KC_RIGHT, KC_RCBR,  KC_BSLS,
+  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F6,    KC_END,  KC_LBRC,    KC_RBRC,    KC_HOME,  KC_LCBR,  KC_RCBR,
+  _______, _______, _______, _______, _______, KC_SPC, KC_ENT, _______,     KC_MNXT,    KC_VOLD,  KC_VOLU,  KC_MPLY),
+
+	[_LOWER] = LAYOUT_ortho(
+  KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_RBRC, KC_PIPE,
+  KC_LSFT,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC,
+  _______, _______, _______, _______, _______, KC_SPC, KC_ENT, _______, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY),
+
+	[_FUNCTION] = LAYOUT_ortho(
+  KC_TILD,  _______, _______, KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_RBRC, KC_PIPE,
+  KC_LSFT,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC,
+  _______, _______, _______, _______, _______, KC_SPC, KC_ENT, _______, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY),
+
+	[_ADJUST] = LAYOUT_ortho(
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL ,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______, _______,  _______,  _______,
+  KC_CAPS, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, KC_SPC, KC_ENT, _______, _______, _______, _______, _______),
+
 
 };
 
@@ -50,9 +78,11 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
 }
 
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
 }
+*/
 
 void led_set_user(uint8_t usb_led) {
 
@@ -120,4 +150,90 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
   }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+	  
+  /*
+	case MACRO_1:
+	  if (record->event.pressed) {
+		SEND_STRING("cd /mnt/c/Users/EJ/Documents");
+		register_code(KC_ENT);
+		return false; 
+	  }
+	case MACRO_2:
+	  if (record->event.pressed) {
+		  //navigate to folder
+		SEND_STRING("cd /mnt/c/Users/EJ/Documents/Github/qmk_lizard");
+		register_code(KC_ENT);
+		unregister_code(KC_ENT);
+		
+		//send command to compile
+		SEND_STRING("make planck:lizard");
+		register_code(KC_ENT);
+		unregister_code(KC_ENT);
+		
+		return false; 
+	  }
+	*/
+	  
+	  
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        #ifdef BACKLIGHT_ENABLE
+          //breathing_period_set(2);
+          //breathing_pulse();
+        #endif
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        //breathing_period_set(2);
+        //breathing_pulse();
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+	  
+	case FUNCTION:
+      if (record->event.pressed) {
+        layer_on(_FUNCTION);
+        //breathing_period_set(2);
+        //breathing_pulse();
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_FUNCTION);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+	  
+  }
+  return true;
 }
