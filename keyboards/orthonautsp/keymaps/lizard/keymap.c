4fcf95f425988d0db20fe3c4c6fdf776b587c71e
noredlace <ejcalderon@gmail.com>
2019-07-19 00:07:25 -0500

    add num lock to keymap

--- keyboards/orthonautsp/keymaps/lizard/keymap.c
+++ keyboards/orthonautsp/keymaps/lizard/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 	[_LOWER] = KEYMAP(
-	 KC_7, KC_8, KC_9,   KC_ESC,   KC_1 ,   KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,  KC_8,   KC_9,    KC_0,     KC_BSPC, KC_TRNS, KC_PSCR,
+	 KC_7, KC_8, KC_9,   KC_ESC,   KC_1 ,   KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,  KC_8,   KC_9,    KC_0,     KC_BSPC, KC_NLCK, KC_PSCR,
    KC_4, KC_5, KC_6,   KC_CTLTB, KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_4,  KC_5,   KC_6,    KC_SCLN,  KC_QUOT, KC_TRNS, KC_TRNS,
    KC_1, KC_2, KC_3,   KC_LSFT,  KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_1,  KC_2,   KC_3,    KC_SLSH,  KC_RSFT, KC_UP,
    KC_DOT, KC_0, KC_0, KC_LCTL,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_SPC,        KC_ENT,    RAISE,    KC_RALT, FUNCTION, KC_LEFT, KC_DOWN, KC_RGHT
