5753f345a0b3a4c4b21f2c2a52cb260fd6d749e7
noredlace <ejcalderon@gmail.com>
2019-09-06 23:12:12 -0500

    add eeprom reset macro. modify enums

--- keyboards/tkc1800/keymaps/lizard/keymap.c
+++ keyboards/tkc1800/keymaps/lizard/keymap.c
@@ -20,17 +20,29 @@
 
 
 //Layers
-
+/*
 enum {
   BASE = 0,
   LOWER = 1,
   FUNCTION,
 };
+*/
+
+enum tkc1800_layers {
+  BASE = 0,
+  LOWER = 1,
+  FUNCTION,
+};
+
+enum tkc1800_keycodes {
+  MACRO_1,
+};
 
 #define KC_LOCT LT(LOWER, KC_CAPS)
 #define KC_MOFU MO(FUNCTION) 
 #define KC_LOWE MO(LOWER) 
-#define KC_CACP CTL_T(KC_CAPS)
+#define KC_CACP CTL_T(KC_CAPS) 
+#define KC_MAC1 MACRO_1
 
 bool screenWorks = 0;
 
@@ -58,13 +70,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------------------------------------------------------------------------------'
    */
   [BASE] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_INS,  KC_HOME, KC_PGUP, RESET, \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_INS,  KC_HOME, KC_PGUP, KC_PSCR, \
                                                                                                                                            KC_DEL,  KC_END,  KC_PGDN, KC_SLCK, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PMNS, \
     KC_CACP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,           KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
     KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,       KC_P1,   KC_P2,   KC_P3,   XXXXXXX, \
-    KC_LOWE, KC_LALT, XXXXXXX,                   KC_SPC,                             XXXXXXX, KC_RALT, KC_MOFU,      KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT  \
+    KC_LCTL, KC_LALT, XXXXXXX,                   KC_SPC,                             XXXXXXX, KC_RGUI, KC_MOFU,      KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT  \
   ),
 
   /* Keymap BASE: (Base Layer) Default Layer
@@ -112,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------------------------------------------------------------------------------'
    */
   [FUNCTION] = LAYOUT(
-       KC_F, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_MAC1, \
                                                                                                                                            _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   XXXXXXX, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, \
@@ -123,6 +135,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+
+	case MACRO_1:
+	  if (record->event.pressed) {
+      eeconfig_init();
+		return false; 
+	  }
+
+  }
   return true;
 }
 
