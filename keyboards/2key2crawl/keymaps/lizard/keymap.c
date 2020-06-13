469489f0aa6f6f6fa91d731f6160ee90141464b2
EJ <ejcalderon@gmail.com>
2020-06-12 23:56:31 -0500

    add keymaps for 2key2crawl and panc40 post fork sync

---
+++ keyboards/2key2crawl/keymaps/lizard/keymap.c
@@ -0,0 +1,191 @@
+#include QMK_KEYBOARD_H
+#include <print.h>
+
+enum crawl_layers {
+  _QWERTY,
+  _VOICEMEETERA1,
+  _VOICEMEETERA2,
+  _VOICEMEETERAAUX,
+  _LOWER,
+  _RAISE,
+  _FUNCTION,
+  _ADJUST
+};
+
+enum crawl_keycodes {
+  QWERTY = SAFE_RANGE,
+  VOICEMEETERA1,
+  VOICEMEETERA2,
+  VOICEMEETERAAUX,
+  LOWER,
+  RAISE,
+  TEST,
+  FUNCTION,
+  MACRO_1,
+  MACRO_2
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[_QWERTY] = KEYMAP(
+    VOICEMEETERA1, VOICEMEETERA2, VOICEMEETERAAUX, KC_F14, RAISE,
+    KC_F15, KC_F16, KC_F17, KC_F18, LOWER, KC_ENT),
+
+//state for voice meeter volume adjust
+   	[_VOICEMEETERA1] = KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT),
+
+    [_VOICEMEETERA2] = KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT),
+
+	[_VOICEMEETERAAUX] = KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT),
+
+	[_LOWER] = KEYMAP(
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_L, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_ENT),
+
+	[_RAISE] = KEYMAP(
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_R, LOWER, KC_BSPC, KC_TRNS, KC_TRNS, KC_ENT),
+
+	[_ADJUST] = KEYMAP(
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_A, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, RESET)
+	
+};
+
+void matrix_init_user(void) {
+  debug_config.matrix = 1;
+  debug_config.keyboard = 1;
+  debug_config.enable = 1;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch(keycode){
+
+		case LOWER:
+			if (record->event.pressed){
+				layer_on(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_LOWER);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+		case RAISE:
+			if (record->event.pressed) {
+				layer_on(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_RAISE);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+		case VOICEMEETERA1:
+			if (record->event.pressed) {
+				layer_on(_VOICEMEETERA1);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_VOICEMEETERA1);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+		case VOICEMEETERA2:
+			if (record->event.pressed) {
+				layer_on(_VOICEMEETERA2);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_VOICEMEETERA2);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+		case VOICEMEETERAAUX:
+			if (record->event.pressed) {
+				layer_on(_VOICEMEETERAAUX);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_VOICEMEETERAAUX);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+
+	}
+	return true;
+}
+
+void encoder_update_user(int8_t index, bool clockwise) {
+
+
+	if (IS_LAYER_ON(_QWERTY)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_PGUP);
+			unregister_code(KC_PGUP);
+			} 
+			else {
+			register_code(KC_PGDN);
+			unregister_code(KC_PGDN);
+			}
+		}
+	}
+
+	//adjust voice meeter output volume
+	if (IS_LAYER_ON(_VOICEMEETERA1)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_F21);
+			unregister_code(KC_F21);
+		} 
+		else {
+			register_code(KC_F22);
+			unregister_code(KC_F22);
+		}
+		}
+	}
+	if (IS_LAYER_ON(_VOICEMEETERA2)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_F23);
+			unregister_code(KC_F23);
+		} 
+		else {
+			register_code(KC_F24);
+			unregister_code(KC_F24);
+		}
+		}
+	}
+	if (IS_LAYER_ON(_VOICEMEETERAAUX)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_F19);
+			unregister_code(KC_F19);
+		} 
+		else {
+			register_code(KC_F20);
+			unregister_code(KC_F20);
+		}
+		}
+	}
+}
+
+
---
+++ keyboards/2key2crawl/keymaps/streetbambi/keymap.c
@@ -0,0 +1,231 @@
+#include QMK_KEYBOARD_H
+
+#define STAR 0x2605
+
+//potential layers for tri state
+enum crawl_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _FUNCTION,
+  _ADJUST
+};
+
+//keycodes and macros
+enum crawl_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  TEST,
+  FUNCTION,
+  SCREENCAP,
+  TOGGLEAPP,
+  NEWTAB,
+  UNDO,
+  QUITAPP,
+  CTRLALTDEL,
+  OPENAPP,
+  COPY,
+  NEWWIN,
+  PASTE,
+  NEWPWIN,
+  SELECT,
+  REFRESH
+};
+
+
+//keymap layout in a similar position to the physical position
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[_QWERTY] = KEYMAP(
+    KC_ESC, NEWTAB, KC_UP, UNDO, KC_DEL,
+    SCREENCAP, KC_LEFT, KC_DOWN, KC_RGHT, LOWER, KC_ENT),
+
+	[_LOWER] = KEYMAP(
+    RESET, SELECT, COPY, PASTE, CTRLALTDEL,
+    QUITAPP, REFRESH, NEWWIN, NEWPWIN, KC_TRNS, KC_ENT)
+
+	/*
+	//no need for raise/adjust layers yet
+	[_RAISE] = KEYMAP(
+    KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_R, KC_TRNS, KC_BSPC, KC_TRNS, KC_0, KC_ENT),
+
+	[_ADJUST] = KEYMAP(
+    KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_A, KC_TRNS, KC_BSPC, KC_TRNS, KC_0, RESET)
+	*/
+};
+
+
+void matrix_init_user(void) {
+  debug_config.matrix = 1;
+  debug_config.keyboard = 1;
+  debug_config.enable = 1;
+  
+  //set aside for emoticon implementation later
+  //set_unicode_input_mode(UC_WINC);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch(keycode){
+		case SCREENCAP:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_LSFT);
+				register_code(KC_4);
+				unregister_code(KC_4);
+				unregister_code(KC_LSFT);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case TOGGLEAPP:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_TAB);
+				unregister_code(KC_TAB);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case NEWTAB:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_T);
+				unregister_code(KC_T);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case UNDO:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_Z);
+				unregister_code(KC_Z);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case QUITAPP:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_Q);
+				unregister_code(KC_Q);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case CTRLALTDEL:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_LALT);
+				register_code(KC_ESC);
+				unregister_code(KC_ESC);
+				unregister_code(KC_LALT);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case OPENAPP:
+			if(record->event.pressed){
+				register_code(KC_LSFT);
+				register_code(KC_LGUI);
+				register_code(KC_A);
+				unregister_code(KC_A);
+				unregister_code(KC_LGUI);
+				unregister_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case COPY:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_C);
+				unregister_code(KC_C);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case NEWWIN:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_N);
+				unregister_code(KC_N);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case PASTE:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_V);
+				unregister_code(KC_V);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case NEWPWIN:
+			if(record->event.pressed){
+				register_code(KC_LSFT);
+				register_code(KC_LGUI);
+				register_code(KC_N);
+				unregister_code(KC_N);
+				unregister_code(KC_LGUI);
+				unregister_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case SELECT:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_A);
+				unregister_code(KC_A);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case REFRESH:
+			if(record->event.pressed){
+				register_code(KC_LGUI);
+				register_code(KC_LSFT);
+				register_code(KC_R);
+				unregister_code(KC_R);
+				unregister_code(KC_LSFT);
+				unregister_code(KC_LGUI);
+			}
+			return false;
+			break;
+		case LOWER:
+			if (record->event.pressed){
+				layer_on(_LOWER);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_LOWER);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+
+	}
+	return true;
+}
+
+void encoder_update_user(int8_t index, bool clockwise) {
+  if (index == 0) {
+    if (clockwise) {
+      register_code(KC_PGUP);
+      unregister_code(KC_PGUP);
+    } else {
+      register_code(KC_PGDOWN);
+      unregister_code(KC_PGDOWN);
+    }
+  }
+  
+  
+}
+
+
+
