e8a2d6578642838f3a1cbe721545890f5c6bb5d3
EJ Calderon <ejcalderon@gmail.com>
2025-01-16 18:59:22 -0600

    idk

--- keyboards/crkbd/keymaps/lizard/keymap.c
+++ keyboards/crkbd/keymaps/lizard/keymap.c
@@ -18,37 +18,93 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum keeb_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _FUNCTION
+};
+
+enum keeb_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  FUNCTION,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
+    [_QWERTY] = LAYOUT_split_3x6_3(
             KC_ESC        , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
             CTL_T(KC_TAB) , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
             KC_LSFT       , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  ,
-                                              KC_LALT   , MO(1)   , KC_SPC  , KC_ENT  , MO(2)   , KC_RGUI
+                                              KC_LALT   , LOWER   , KC_SPC  , KC_ENT  , RAISE   , KC_RGUI
 
   ),
 
-    [1] = LAYOUT_split_3x6_3(
+    [_LOWER] = LAYOUT_split_3x6_3(
             KC_ESC       , KC_1     , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
             CTL_T(KC_TAB), KC_F1    , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_4    , KC_5    , KC_6    , KC_SCLN , KC_QUOT ,
-            KC_LSFT      , KC_F7    , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_1    , KC_2    , KC_3    , KC_SLSH , KC_RSFT ,
-                                               KC_LALT  , _______ , KC_SPC  , KC_ENT  , MO(3)   , KC_RGUI
+            KC_LSFT      , KC_F7    , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_1    , KC_2    , KC_3    , KC_SLSH , KC_PSCR ,
+                                               KC_LALT  , _______ , KC_SPC  , KC_ENT  , FUNCTION   , KC_RGUI
   ),
 
-    [2] = LAYOUT_split_3x6_3(
+    [_RAISE] = LAYOUT_split_3x6_3(
             KC_ESC        , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSPC ,
             CTL_T(KC_TAB) , KC_HOME , KC_MINS , KC_EQL  , KC_LCBR , KC_RCBR , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT, KC_PIPE , KC_GRV,
             KC_LSFT       , KC_END  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_UNDS , KC_PLUS , KC_LBRC , KC_RBRC , KC_BSLS , KC_TILD,
-                                                KC_LALT , MO(3)   , KC_SPC  , KC_ENT  , _______ , KC_RGUI
+                                                KC_LALT , FUNCTION   , KC_SPC  , KC_ENT  , _______ , KC_RGUI
   ),
 
-    [3] = LAYOUT_split_3x6_3(
+    [_FUNCTION] = LAYOUT_split_3x6_3(
             QK_BOOT , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_DEL  ,
             RGB_TOG , RGB_HUI , RGB_SAI , RGB_VAI , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
-            RGB_MOD , RGB_HUD , RGB_SAD , RGB_VAD , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PSCR ,
+            RGB_MOD , RGB_HUD , RGB_SAD , RGB_VAD , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
                                           KC_LALT , _______ , KC_SPC  , KC_ENT  , _______ , KC_RGUI
   )
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+
+	case FUNCTION:
+      if (record->event.pressed) {
+        layer_on(_FUNCTION);
+      } else {
+        layer_off(_FUNCTION);
+      }
+      return false;
+      break;
+
+    }
+    return true;
+}
 
 #ifdef OLED_ENABLE
 
